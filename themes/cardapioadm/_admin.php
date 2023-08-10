49580e4453b6a512c6e89bddeb0bf425b78d966b
RafaelBlum <Rafaelblum_digital@hotmail.com>
2023-08-09 23:28:43 -0300

    docs: ajuste README

--- themes/cardapioadm/_admin.php
+++ themes/cardapioadm/_admin.php
@@ -89,7 +89,10 @@
             <div class="dash_userbar_box">
                 <!--========== CONTENT BAR ==========-->
                 <div class="dash_content_box">
-                    <h1><img src="<?= image("images/favicon.png", 25) ?>" alt="about"> <a href="<?= url("/admin/dash"); ?>">Cardapio<b>Admin</b></a></h1>
+                    <h1>
+                        <img src="<?= image("images/favicon.png", 25) ?>" alt="about">
+                        <a href="<?= url("/admin/dash"); ?>">Cardapio<b>Admin</b></a>
+                    </h1>
                     <div class="dash_userbar_box_bar">
                         <span class="no_mobile icon-clock-o"><?= date("d/m H\hi"); ?></span>
                         <a class="no_mobile icon-sign-out" title="Sair" href="<?= url("/admin/logoff"); ?>">Sair</a>
